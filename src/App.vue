0e9ae8882fda50586ed4c7f8105540fd31a1914b
mark.dowton <mark.dowton@bigpicturemedical.com>
2024-07-09 12:01:22 +1000

    Dev site

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// const items = [
+//   {
+//     title: "Byron Bay The Pass",
+//     value: 1,
+//   },
+//   {
+//     title: "Byron Bay Main Beach",
+//     value: 2,
+//   },
+//   {
+//     title: "Ballina River",
+//     value: 3,
+//   },
+// ];
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="main-container rounded rounded-md">
+    <div class="sidebar">
+      <img src="./assets/Mark Dowton.png" alt="main-logo" class="w-full" />
+      <v-list lines="three" class="text-left bg-none">
+        <v-list-item
+          title="Address"
+          subtitle="5 Talbot Close McLeans Ridges NSW 2480"
+        ></v-list-item>
+        <v-list-item title="Phone" subtitle="0407 108 787"></v-list-item>
+        <v-list-item
+          title="Email"
+          subtitle="markdowton007@gmail.com"
+        ></v-list-item>
+        <v-list-item
+          title="LinkedIn"
+          subtitle="https://www.linkedin.com/in/mark-dowton-03a85a41/"
+        ></v-list-item>
+      </v-list>
+    </div>
+
+    <div class="main-section">
+      <h1 class="text-white text-6xl z-10 absolute bottom-0 left-0 ml-5 mb-10">
+        DEVELOPER
+      </h1>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -26,5 +56,54 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.main-container {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+}
+.sidebar {
+  width: 360px;
+  background-color: #183c67;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+}
+
+.sidebar a {
+  color: white;
+  text-decoration: none;
+  margin-bottom: 10px;
+  padding: 10px;
+  transition: background 0.3s;
+}
+
+.sidebar a:hover {
+  background-color: #575757;
+}
+.main-section {
+  flex-grow: 1;
+  top: 0;
+  left: 0;
+  background: url("./assets/marks_profile_image.png") no-repeat 260px center
+    fixed;
+  background-size: cover;
+  position: relative;
+}
+.main-section::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3); /* Black transparent overlay */
+  z-index: 0;
+}
+.v-list {
+  background: none;
+  color: white;
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,vue}"],
+  theme: {
+    colors: {
+      'bg-theme-blue': '#183c67',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
